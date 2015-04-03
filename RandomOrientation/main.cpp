65abe0b48c7e63391e5cb2aee7d9d07792134985
Alexander Konoshonkin <sasha_tvo@iao.ru>
2015-04-03 16:50:27 +0600

    Приведен к тому виду, как было описано в статьях

--- RandomOrientation/main.cpp
+++ RandomOrientation/main.cpp
@@ -10,10 +10,13 @@
 #include <time.h>
 #include <math.h>
 
+//Axilary functions
 #include "..\Lib\trajectory.hpp"
 #include "..\Lib\PhysMtr.hpp"
 #include "..\Lib\Mueller.hpp"
 
+
+//The beam-splitting
 #include "..\Lib\particle.hpp"
 
 
@@ -27,7 +30,8 @@ unsigned int 	KoP,						///< Kind of particle
 				BettaNumber,				///< Number of steps for Betta rotation
 				ThetaNumber,				///< Total number of bins in output file
 				Itr,						///< Maximal number of internal reflection for every beam
-				EDF;						///< EDF = 1 means to extract the delta-function in forward direction, EDF = 0 otherwise
+				EDF,						///< EDF = 1 means to extract the delta-function in forward direction, EDF = 0 otherwise
+				isNotRandom;				///< if 1 particle rotates by discret steps, if 0 particle rotates with random function
 complex 		_RefI(0,0);					///< Refraction index
 unsigned int 	Sorting=0,					///< Sorting = 0 if all trajectories are taking into account, otherwise Sorting is equal to number of trajectories to calculate
 				_NoF,						///< Number of facets of the crystal
@@ -45,7 +49,7 @@ list<Chain> mask;							///< List of trajectories to take into account
 Point3D			k(0,0,1),					///< Direction on incident wave
 				Ey(0,1,0);					///< Basis for polarization characteristic of light
 //==============================================================================
-///< handler for the emitted beams
+///< Handler for the emitted beams
 void  Handler(Beam& bm);
 //==============================================================================
 
@@ -64,7 +68,7 @@ void DelFace(void);
 /// Main()
 int main(int argc, char* argv[])
 {
-    srand (time(NULL));
+	srand (time(NULL));
 	QCoreApplication a(argc, argv); ///< QT needs it
 	ShowTitle();
 	cout << "\nLoading settings... ";
@@ -78,7 +82,7 @@ int main(int argc, char* argv[])
 
 	unsigned int NumberOfParameters = 13;	///< Number of lines in data files, except trajectories
 	double params[NumberOfParameters];		///< array of input data	
-	// read parameters from data file
+	// Read parameters from data file
 	try
 	{
 		if(ReadFile((char*)"params.dat", params, NumberOfParameters)) {
@@ -90,6 +94,7 @@ int main(int argc, char* argv[])
 		cout << endl << s << "\nPress any key.";
 		getch(); return 1;
 	}
+	//Assign the parameters from data file.
 	//---------------------------------------------------------------------------
 	KoP = 			params[0];
 	AoP56 = 		params[1];
@@ -97,17 +102,19 @@ int main(int argc, char* argv[])
 	Halh_Height = 	params[3];
 	TipRadius = 	params[4];
 	TipHeight = 	params[5];
-	_RefI =			complex(params[6],params[7]);
-	GammaNumber = 	params[8];
-	BettaNumber = 	params[9];
-	ThetaNumber = 	params[10];
-	Itr = 			params[11];
-	EDF = 			params[12];
+	_RefI =			complex(params[6],0);
+	GammaNumber = 	params[7];
+	BettaNumber = 	params[8];
+	ThetaNumber = 	params[9];
+	Itr = 			params[10];
+	EDF = 			params[11];
+	isNotRandom =	params[12];
+
 	//----------------------------------------------------------------------------
 	double  
 		NormAng = 			sqrt(3.0)/(2.0*tan(0.48869219055841228153863341517681)), 
-		NormGammaAngle = 	0,
-		NormBettaAngle =  	0;
+		NormGammaAngle =	0,
+		NormBettaAngle =	0;
 		SizeBin = 			M_PI/ThetaNumber; // the size of the bin (radians)
 	//----------------------------------------------------------------------------
 	double hp;
@@ -146,42 +153,46 @@ int main(int argc, char* argv[])
 		
 	}
 
+	// the arrays for exact backscattring and forwardscattering Mueller matrices
 	back.Fill(0); forw.Fill(0);
+
+	// the array for the Mueller matrices dependent from zenith angle
 	mxd = Arr2D(1, ThetaNumber+1, 4, 4);
 	mxd.ClearArr();
 
 	Body->Phase() = false;
 	const double NumOrient = GammaNumber*BettaNumber;
+
+
+	// The main loop
 	//----------------------------------------------------------------------------
-	#ifdef _Windows
-		gotoxy(1,6);  ShowCurrentTime();   // current time (Win32 API)
-	#endif
-    clock_t t = clock();
-    cout << BettaNumber;
-	//----------------------------------------------------------------------------
+
+	clock_t t = clock();
+	cout << "\nPercent done";
 	double s = 0, tetta, gamma;
 	try {
 		for(unsigned int i=0; i<BettaNumber; i++) {
 			if(!i) cout << "\n" << 0;
-            //tetta = (i+0.5)*NormBettaAngle;
-            for(unsigned int j=0; j<GammaNumber; j++) {
-                //gamma = (j+0.5)*NormGammaAngle;
-                gamma=2.0*M_PI*((double)(rand()) / ((double)RAND_MAX+1.0));
-                tetta=acos(((double)(rand()) / ((double)RAND_MAX))*2.0-1.0);
-                //tetta=2.0*M_PI/3.0;
-                //gamma=M_PI/6.0;
-                //tetta=10*M_PI/180.0;
-
+			if (isNotRandom)
+				tetta = (i+0.5)*NormBettaAngle;
+			for(unsigned int j=0; j<GammaNumber; j++) {
+				if (isNotRandom)
+					gamma = (j+0.5)*NormGammaAngle;
+				else
+				{
+					gamma=2.0*M_PI*((double)(rand()) / ((double)RAND_MAX+1.0));
+					tetta=acos(((double)(rand()) / ((double)RAND_MAX))*2.0-1.0);
+				}
 				Body->ChangePosition(tetta, gamma,0.0);
-                //P = sin(tetta);
-                P=1.0;
+				//P = sin(tetta);
+				P=1.0;
 				s += P*Body->FTforConvexCrystal(Handler);
 				if(!(j%100)) cout<<'.';
 			}
 			if(!KoP || KoP==3)
-				cout << "\n" << (1-cos(tetta));
+				cout << "\n" << (i+0.5)*NormBettaAngle/M_PI*200;
 			else
-				cout << "\n" << (1-cos(tetta))/2.0;
+				cout << "\n" << (i+0.5)*NormBettaAngle/M_PI*100;
 		}
 	}
 	catch(char* s) {
@@ -189,33 +200,40 @@ int main(int argc, char* argv[])
 		getch(); return 1;
 	}
 	t = clock()-t;
+	// End of the main loop
+	//----------------------------------------------------------------------------
 
 	cout << "\nTotal time of calculation = " << t/CLK_TCK << " seconds";
-	char text1[] = "M11 m22 m33 m44",
-	text2[] = "tetta M11 m12 m21 m22 m33 m34 m43 m44";
+
+	//Analitical averaging over alpha angle
 	//----------------------------------------------------------------------------
-	double b[3], f[3];
+	double b[3], f[3];	
 	b[0] = back[0][0]; b[1] = (back[1][1]-back[2][2])/2.; b[2] = back[3][3];
 	f[0] = forw[0][0]; f[1] = (forw[1][1]+forw[2][2])/2.; f[2] = forw[3][3];
+
+	//Integrating
 	//----------------------------------------------------------------------------
 	double D_tot = b[0]+f[0];
 	for(int j=0; j<=ThetaNumber; j++)
-		D_tot += mxd(0,j,0,0);
+		D_tot += mxd(0,j,0,0);	
+
+	//Normalizing coefficient
+	const double NRM = 1.0/(4.0*M_PI);
+
+	// Extracting the forward and backward peak in a separate file if needed
 	//----------------------------------------------------------------------------
 	if(EDF) {
 		ofstream bck("back.dat", ios::out), frw("forward.dat", ios::out);
-		for(unsigned int j=0;j<3;j++) {
-			f[j] /= D_tot; b[j] /= D_tot;
-		}
-		frw << text1; bck << text1;
+		frw << "M11 m22 m33 m44";
+		bck << "M11 m22 m33 m44";
 		if(f[0]<=DBL_EPSILON)
 			frw << "\n0 0 0 0";
 		else
-			frw << "\n" << f[0] << " " << f[1]/f[0] << " " <<  f[1]/f[0] << " " << f[2]/f[0];
+			frw << "\n" << f[0]/(NRM*(1-cos(SizeBin/2.0))*D_tot) << " " << f[1]/f[0] << " " <<  f[1]/f[0] << " " << f[2]/f[0];
 		if(b[0]<=DBL_EPSILON)
 			bck << "\n0 0 0 0";
 		else
-			bck << "\n" << b[0] << " " << b[1]/b[0] << " " << -b[1]/b[0] << " " << b[2]/b[0];
+			bck << "\n" << b[0]/(NRM*(1-cos(SizeBin/2.0))*D_tot) << " " << b[1]/b[0] << " " << -b[1]/b[0] << " " << b[2]/b[0];
 		bck.close();
 		frw.close();
 	}
@@ -226,27 +244,27 @@ int main(int argc, char* argv[])
 		mxd(0,ThetaNumber,3,3) += f[2]; mxd(0,0,3,3) += b[2];
 	}
 	//----------------------------------------------------------------------------
-	D_tot /= 2.0*NumOrient/M_PI;
-    const double NRM = 1.0*NumOrient;
+
+	//Output the matrices
 	ofstream M("M.dat", ios::out), out("out.dat", ios::out);
-	M << text2;
+	M <<  "tetta M11 m12 m21 m22 m33 m34 m43 m44";
 	for(int j=ThetaNumber;j>=0;j--) {
 		double sn;
-		M << '\n' << 180.0/ThetaNumber*(ThetaNumber-j);
+		//Special case in first and last step
+		M << '\n' << 180.0/ThetaNumber*(ThetaNumber-j)+(j==0 ?-0.25*180.0/ThetaNumber:0)+(j==ThetaNumber ?0.25*180.0/ThetaNumber:0);
 		sn = (j==0 || j==ThetaNumber) ? 1-cos(SizeBin/2.0) : (cos((j-0.5)*SizeBin)-cos((j+0.5)*SizeBin));
+
 		matrix bf = mxd(0,j);
 		if(bf[0][0] <= DBL_EPSILON)
 			M << " 0 0 0 0 0 0 0 0";
-		else
-            //M<<' '<<bf[0][0]<<' '
-            M<<' '<<bf[0][0]/(NRM*sn*D_tot)*M_PI<<' '
-            <<' '<<bf[0][0]/(NRM*sn)<<' '
+		else            
+			M<<' '<<bf[0][0]/(NRM*sn*D_tot)<<' '
 			<<bf[0][1]/bf[0][0]<<' '<<bf[1][0]/bf[0][0]<<' '<<bf[1][1]/bf[0][0]<<' '
 			<<bf[2][2]/bf[0][0]<<' '<<bf[2][3]/bf[0][0]<<' '<<bf[3][2]/bf[0][0]<<' '<<bf[3][3]/bf[0][0];
 	}
 	M.close();
 	//----------------------------------------------------------------------------
-	// information for log-file
+	// Information for log-file
 	out << "\nTotal time of calculation = " << t/CLK_TCK << " seconds";
 	out << "\nTotal number of insident photons = " << s;
 	out << "\nTotal number of body orientation = " << NumOrient;
@@ -268,26 +286,54 @@ int main(int argc, char* argv[])
 }
 //==============================================================================
 
-
+//The function that takes outgoing beams into account
 void  Handler(Beam& bm)
 {
+	uint  pn = 0, szP = SizeP(bm);
+	uint vi=0;
+	// If there is a special list of the beams to take into account, check if the beam "bm" is on the list
+	if (Sorting)
+	{		
+		bool flag = false;
+		list<Chain>::const_iterator c = mask.begin();
+		for (;c!=mask.end();c++)
+		{
+			list<uint >::const_iterator it = c->Ch.begin();
+			if (szP!=c->sz) continue;
+			list<uint >::const_iterator fs = bm.BeginP();
+			for (;it!=c->Ch.end() && (*it)==(*fs);it++, fs++);
+			if (it==c->Ch.end())
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (!flag) return;
+	}
+
+
 	double Area = P*CrossSection(bm);
 	matrix bf = Mueller(bm());
 	//----------------------------------------------------------------------------
-	if(bm.r.z >= 1-DBL_EPSILON) {
+	// Collect the beam in array
+	const unsigned int ZenAng = round(acos(bm.r.z)/SizeBin);
+	if (ZenAng==0)
+	{
 		back += Area*bf;
 	}
-	else 
-		if(bm.r.z <= DBL_EPSILON-1) {
+	else 		
+		if (ZenAng==ThetaNumber)
+		{
 			forw += Area*bf;
 		}
 		else {
-			const unsigned int ZenAng = round(acos(bm.r.z)/SizeBin);
+			// Rotate the Mueller matrix of the beam to appropriate coordinate system
+
 			double tmp = SQR(bm.r.y);
 			if(tmp > DBL_EPSILON) {
 				tmp = acos(bm.r.x/sqrt(SQR(bm.r.x)+tmp));
 				if(bm.r.y<0) tmp = m_2pi-tmp;
-                    tmp *= -2.0;
+				tmp *= -2.0;
 				double cs = cos(tmp), sn = sin(tmp);
 				RightRotateMueller(bf, cs, sn);
 			}
@@ -300,14 +346,15 @@ const int size = 256;
 
 int ReadFile(char* name, double* params, unsigned int n)
 {
-	char buf[size]=""; //буфер
-	ifstream in(name, ios::in); //входной файл
+	char buf[size]=""; //temp buffer
+	ifstream in(name, ios::in);
 	for(unsigned int i=0; i<n; i++) {
 		if(in.eof()) return 1;
 		in.getline(buf, size);
 		params[i] = strtod(buf, NULL);
 		if (params[i]<0) throw "All values must be positive in data file";
 	}
+	//Choose the _NoF (Number of facets) according to the chosen kind of particle
 	//----------------------------------------------------------------------------
 	switch(int(params[0])) {
 		case 0: _NoF =  8; break;
@@ -319,10 +366,12 @@ int ReadFile(char* name, double* params, unsigned int n)
 		case 6: _NoF = 4; break;
 		case 7: _NoF = 5; break;
 	}
+	Itr = params[11];
 
 	if(!in.eof()) {
 	in.getline(buf, size);
 	Sorting = strtod(buf, NULL);
+	// If there is a special list of beams
 	if(Sorting>0) {
 		Face = new unsigned int*[int(Itr)+1];
 		for(unsigned int i=0;i<Itr+1; i++)
@@ -335,7 +384,7 @@ int ReadFile(char* name, double* params, unsigned int n)
 		for(unsigned int j=0;j<Sorting;j++) {
 			if(in.eof()) return 1;
 			in.getline(buf, size);
-			MaskAppend(buf,size);
+			MaskAppend(buf,size); //Add the beam into special list
 		}
 	}
 	}
@@ -344,6 +393,7 @@ int ReadFile(char* name, double* params, unsigned int n)
 }
 
 
+//This function interprets the text line into beam name and bushed this name into special list
 void MaskAppend(char s[], unsigned int n)
 {
 	list<unsigned int> ch;
@@ -370,9 +420,9 @@ void MaskAppend(char s[], unsigned int n)
 
 void DelFace(void)
 {
-    for(unsigned int i=0;i<_NoF; i++)
-        delete[] Face[i];
-    delete[] Face;
+	for(unsigned int i=0;i<Itr+1; i++)
+		delete[] Face[i];
+	delete[] Face;
 }
 
 void ShowTitle(void)
@@ -386,11 +436,4 @@ void ShowTitle(void)
 
 //==============================================================================
 
-void ShowCurrentTime(void)
- {
-/*  using namespace std;
-  SYSTEMTIME Time;
-  GetLocalTime(&Time);
-  cout << "Numerical calculations started at: " <<
-          Time.wHour << ':' << Time.wMinute << ':' << Time.wSecond;*/
- }
+
